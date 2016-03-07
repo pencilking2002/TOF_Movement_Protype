a87ac6e34cba86af7a2683d15401ec6a710e055b
Roman <pencilking2002@gmail.com>
2016-03-07 10:01:57 -0500

    Updated PhysicsMaterialHandler script to account for walls

--- Suzie_Movement_Final2/Assets/Scripts/PhysicMaterialHandler.cs
+++ Suzie_Movement_Final2/Assets/Scripts/PhysicMaterialHandler.cs
@@ -45,26 +45,22 @@ public class PhysicMaterialHandler : MonoBehaviour {
 
 	private void Update ()
 	{
-		// Raycast into the ground and make sure its a ground material
-//		origin = transform.position;
-//		ray = new Ray(origin, Vector3.down);
-//		if ((GameManager.Instance.charState.IsIdleOrRunning()) && 
-//		    Physics.Raycast (ray, out hit, groundRayLenth) && hit.collider.material != groundMaterial)
-//		{
-//			hit.collider.material = groundMaterial;
-//		}
+		if (GameManager.Instance.charState.IsInAnyJumpingState())
+		{
+			// Set the origin of the ray, which will be 
+			// coming out of the middle of the character (vertially)
+			origin = transform.position;
+			origin.y = transform.position.y + transform.lossyScale.y / 2;
+			ray = new Ray(origin, transform.forward);
 
-//		if (GameManager.Instance.charState.IsIdleOrRunningJumping())
-//		{
-//			origin = transform.position;
-//			origin.y = transform.position.y + 1;
-//			ray = new Ray(origin, transform.forward);
-//
-//			if (Physics.Raycast (ray, out hit, groundRayLenth) && hit.collider.material != groundMaterial)
-//			{
-//				
-//			}
-//		}
+			Debug.DrawRay(ray.origin, ray.direction * wallRayLength, Color.green);
+
+			if (Physics.Raycast (ray, out hit, wallRayLength))
+			{
+				hit.collider.material = wallMaterial;
+				//Debug.Break();
+			}
+		}
 	}
 
 	private void OnEnable () 
@@ -79,12 +75,14 @@ public class PhysicMaterialHandler : MonoBehaviour {
 		EventManager.onCharEvent -= SetPhysicMaterial;
 	}
 
-	// TODO get rid of land code?
+	/// <summary>
+	/// When the character lands, set the mesh underneath to a ground physics material
+	/// </summary>
+	/// <param name="gEvent">G event.</param>
 	private void SetPhysicMaterial(GameEvent gEvent)
 	{
 		if (gEvent == GameEvent.Land)
 		{
-			
 			//origin = cCollider.bounds.center - cCollider.bounds.extents;
 			origin = transform.position;
 			ray = new Ray(origin, Vector3.down);
@@ -97,49 +95,7 @@ public class PhysicMaterialHandler : MonoBehaviour {
 				print ("is on ground");
 				hit.collider.material = groundMaterial;
 				GameManager.Instance.charState.SetState(RomanCharState.State.Idle);
-				
 			}
 		}
-		else if (gEvent == GameEvent.WallCollision)
-		{
-			origin = cCollider.bounds.center;
-			Vector3 bottomOrigin = transform.position;
-
-			ray = new Ray(origin, transform.forward);
-			rayFromBottom = new Ray(bottomOrigin, transform.forward);
-//			
-//			Debug.LogError("blah");
-			if (Physics.Raycast (ray, out hit, wallRayLength) || Physics.Raycast (rayFromBottom, out hit, wallRayLength))
-			{
-				Debug.DrawLine (origin, hit.point, Color.green);
-				Debug.DrawLine (bottomOrigin, hit.point, Color.green);
-
-				hit.collider.material = wallMaterial;
-				//print ("Wall Collision");
-				//Debug.Break();
-			}
-
-
-		}
-		
 	}
-	private void OnCollisionEnter(Collision other)
-	{
-		if (other.gameObject.layer == 9)
-			return;
-
-		origin = transform.position;
-		origin.y = transform.position.y + 0.5f;
-		Vector3 pointOfContact = new Vector3(other.contacts[0].point.x, origin.y, other.contacts[0].point.z);
-
-		//print(Vector3.Dot((pointOfContact - origin).normalized, transform.forward.normalized));
-
-		Debug.DrawLine(origin, pointOfContact, Color.black);
-		//Debug.Break();	
-	}
-//	private void SetPhysicMaterial(Collider col, bool ground)
-//	{
-//		col.material = ground ? groundMaterial : wallMaterial;
-//	}
-	
 }
