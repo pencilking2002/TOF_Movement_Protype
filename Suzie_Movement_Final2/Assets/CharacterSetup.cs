c2c38d9e8ad0dbe694941e1f97a283c6092cf448
Roman <pencilking2002@gmail.com>
2016-03-16 16:00:06 -0400

    Created a CharacterSetup script which unparents the Follow and Cam on Awake()

---
+++ Suzie_Movement_Final2/Assets/CharacterSetup.cs
@@ -0,0 +1,22 @@
+﻿// Class to setup the follow object and the camera.
+// By default they are parented to the character for easy positioning in the scene
+// Whwn the game starts his class unparents them
+using UnityEngine;
+using System.Collections;
+
+public class CharacterSetup : MonoBehaviour {
+
+	public Transform cam;
+	public Transform follow;
+
+	void Awake()
+	{
+		cam.SetParent(null);
+		follow.SetParent(null);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
