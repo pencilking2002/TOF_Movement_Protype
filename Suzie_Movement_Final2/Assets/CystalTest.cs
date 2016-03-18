6c28e89a5060ec101f4d3248ad65520bbddb145e
Roman <pencilking2002@gmail.com>
2016-03-18 11:50:17 -0400

    Added standard assets

---
+++ Suzie_Movement_Final2/Assets/CystalTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CystalTest : MonoBehaviour {
+
+	private MeshRenderer rend;
+	// Use this for initialization
+	void Start () 
+	{
+		rend = GetComponent<MeshRenderer>();
+		rend.sharedMaterial.SetInt("_ZWrite", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
