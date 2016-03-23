8e0bc56b41e9fba6af71a9a304b6645b230ae77f
Roman <pencilking2002@gmail.com>
2016-03-23 12:20:06 -0400

    Optimized nut collecting code

---
+++ Suzie_Movement_Final2/Assets/NutCollector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NutCollector : MonoBehaviour 
+{
+	[Range(0,10)]
+	public float scaleTime = 2.0f;
+
+	[Range(0,1)]
+	public float scaleUpTime = 0.1f;
+
+	[Range(0,10)]
+	public float moveDamp = 2.0f;
+
+	public Vector3 scaleUpVector = new Vector3(1.4f, 1.4f, 1.4f); 
+	private Vector3 moveVel;
+
+	private void OnTriggerStay (Collider col)
+	{
+		if (col.gameObject.layer == 15)
+		{
+			GameObject pickup = col.gameObject;
+
+			print("nut triggered");
+			// Switch pickup to a layer that ignores all player collisions
+			pickup.layer = 16;
+
+			LeanTween.scale(pickup, scaleUpVector,scaleUpTime)
+			.setOnComplete(() => {
+				LeanTween.scale(pickup, Vector3.zero, scaleTime)
+					.setEase(LeanTweenType.easeInBack)
+					.setOnUpdate((float pos) => {
+						pickup.transform.position = Vector3.SmoothDamp(pickup.transform.position, transform.position, ref moveVel, moveDamp);	
+				})
+				.setOnComplete(() => {
+					pickup.SetActive(false);
+				});
+			});
+		}
+	}
+}
--- Suzie_Movement_Final2/Assets/PickupItem.cs
+++ Suzie_Movement_Final2/Assets/PickupItem.cs
@@ -3,42 +3,48 @@ using System.Collections;
 
 public class PickupItem : MonoBehaviour {
 
-	[Range(0, 10)]
-	public float scaleSpeed = 0.6f;
-	[Range(0, 10)]
-	public float moveDamp = 0.5f;
-
-	private Vector3 initialScale;
+//	[Range(0, 10)]
+//	public float scaleSpeed = 0.6f;
+//	[Range(0, 10)]
+//	public float moveDamp = 0.5f;
+//
+//	private Vector3 initialScale;
 	private bool isPickedUp;
-	private GameObject parentGO;
-	private Transform player;
-	private Vector3 targetPos;
-	private Vector3 moveVel;
 
-	// Use this for initialization
-	void Start () 
+	public void PickupUp(bool pickedUp)
 	{
-		initialScale = transform.localScale;
-		parentGO = transform.parent.gameObject;
-		//transform.localScale = Vector3.zero;
+		isPickedUp = pickedUp;
 	}
 
-	private void OnTriggerStay (Collider col)
-	{
-		if (col.gameObject.layer == 8 && !isPickedUp)
-		{
-			player = col.transform;
-			//targetPos = player.position;
-
-			isPickedUp = true;
-			LeanTween.scale(parentGO, Vector3.zero, scaleSpeed)
-				.setEase(LeanTweenType.easeInBack)
-				.setOnUpdate((float pos) => {
-					parentGO.transform.position = Vector3.SmoothDamp(parentGO.transform.position, player.position, ref moveVel, moveDamp);	
-			})
-			.setOnComplete(() => {
-				parentGO.SetActive(false);
-			});
-		}
-	}
+//	private GameObject parentGO;
+//	private Transform player;
+//	private Vector3 targetPos;
+//	private Vector3 moveVel;
+//
+//	// Use this for initialization
+//	void Start () 
+//	{
+//		initialScale = transform.localScale;
+//		parentGO = transform.parent.gameObject;
+//		//transform.localScale = Vector3.zero;
+//	}
+//
+//	private void OnTriggerStay (Collider col)
+//	{
+//		if (col.gameObject.layer == 8 && !isPickedUp)
+//		{
+//			player = col.transform;
+//			//targetPos = player.position;
+//
+//			isPickedUp = true;
+//			LeanTween.scale(parentGO, Vector3.zero, scaleSpeed)
+//				.setEase(LeanTweenType.easeInBack)
+//				.setOnUpdate((float pos) => {
+//					parentGO.transform.position = Vector3.SmoothDamp(parentGO.transform.position, player.position, ref moveVel, moveDamp);	
+//			})
+//			.setOnComplete(() => {
+//				parentGO.SetActive(false);
+//			});
+//		}
+//	}
 }
