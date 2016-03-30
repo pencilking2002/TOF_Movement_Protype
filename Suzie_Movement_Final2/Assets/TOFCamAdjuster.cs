43e269923eb8e9cee511952b6d76798d6c888e3f
Roman <pencilking2002@gmail.com>
2016-03-29 23:52:24 -0400

    soundtrack

---
+++ Suzie_Movement_Final2/Assets/TOFCamAdjuster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TOFCamAdjuster : MonoBehaviour {
+
+	private PhatRobit.SimpleRpgCamera rpgCam;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		rpgCam = GetComponent<PhatRobit.SimpleRpgCamera>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if (
+	}
+}
