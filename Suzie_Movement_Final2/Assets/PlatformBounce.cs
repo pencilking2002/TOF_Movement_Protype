831c41ee0d7cf0846af887cf4505413e79c78429
Roman <pencilking2002@gmail.com>
2016-04-01 12:03:30 -0400

    Bouncing platform tests

---
+++ Suzie_Movement_Final2/Assets/PlatformBounce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformBounce : MonoBehaviour {	
+
+	Rigidbody rb;
+
+	void Awake()
+	{
+		//rb = GetComponent<Rigidbody>();
+	}
+
+	void OnCollisionEnter()
+	{
+		//rb.isKinematic = false;
+	}
+}
