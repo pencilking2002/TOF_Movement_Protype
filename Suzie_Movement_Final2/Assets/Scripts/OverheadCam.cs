bd90a2bb702943703e81829c13c262c5837917df
Roman <pencilking2002@gmail.com>
2016-05-12 22:16:34 -0400

    Changed camera and put in some platforms that are parallel to the camera

--- Suzie_Movement_Final2/Assets/Scripts/OverheadCam.cs
+++ Suzie_Movement_Final2/Assets/Scripts/OverheadCam.cs
@@ -2,56 +2,47 @@
 using System.Collections;
 
 public class OverheadCam : MonoBehaviour {
+	public Transform Squirrel;
 
-	//Vector3 offset;
-	public float distanceAway = 2.0f;
-	public bool smooth = true;
-	public float smoothDamp = 2.0f;
-	public float rotSpeed = 20.0f;
+	public Vector3 offfset = new Vector3(0, 8.0f, 7.0f);				// Offset of the cam
+	public float turnSpeed = 4.0f;
+	public float xSpeed = 20.0f;
+	public float damping = 0.02f;
+	public bool canRotate = true;
 
-	Transform Squirrel;
-	RomanCharController charController;
-	Vector3 constantDistance;
-	float camYPos;
-	Vector3 smoothVel;
+	private Vector3 vel;
+	private Quaternion lastRot;
+	private float x;
+
 
 	// Use this for initialization
 	void Start () 
 	{
-		transform.LookAt (Squirrel);
-		charController = GameManager.Instance.charController;
-		Squirrel = charController.transform;
-
-		constantDistance = (transform.position - Squirrel.position ).normalized;
-
-		camYPos = transform.position.y;
-
-		PositionCamera ();
+		//transform.rotation =  Quaternion.Euler(0, transform.eulerAngles.y, 0);
+		RotateCam ();
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () 
 	{
-		PositionCamera ();
+		RotateCam ();
 	}
 
-	void PositionCamera()
+	private void RotateCam()
 	{
-		if (smooth) 
-		{
-			Vector3 targetPos = Squirrel.position + constantDistance * distanceAway;
-			//targetPos.y = camYPos;
-			//transform.position = targetPos;
-			transform.position = Vector3.SmoothDamp (transform.position, targetPos, ref smoothVel, smoothDamp);
-			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (Squirrel.position - transform.position), rotSpeed * Time.deltaTime);
-			//transform.LookAt (Squirrel);
-		} 
-		else 
-		{
-			Vector3 targetPos = Squirrel.position + constantDistance * distanceAway;
-			//targetPos.y = camYPos;
-			transform.position = targetPos;
-			transform.LookAt (Squirrel);
-		}
+
+		// Increment the x-axis input
+		x = InputController.orbitH * xSpeed;
+		//x = Mathf.Atan ();
+		//Rotate the camera to those angles 
+		Quaternion rotation = Quaternion.Euler(new Vector3(0, x + transform.eulerAngles.y, 0));
+
+		transform.rotation = rotation;
+
+		//Move the camera to look at the target
+		Vector3 position = rotation * new Vector3(0.0f, offfset.y, -offfset.z) + Squirrel.position;
+		transform.position = Vector3.SmoothDamp (transform.position, position, ref vel, damping);
+		transform.LookAt (Squirrel);
 	}
+		
 }
