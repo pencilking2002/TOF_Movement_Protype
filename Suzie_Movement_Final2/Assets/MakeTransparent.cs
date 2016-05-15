90b904bafb48e06b8097f59af49369a66d604247
Roman <pencilking2002@gmail.com>
2016-05-15 16:02:53 -0400

    Ability for meshes to be transparent when they block the character form the camera

---
+++ Suzie_Movement_Final2/Assets/MakeTransparent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeTransparent : MonoBehaviour {
+	[HideInInspector]
+	public Transform Squirrel;
+	public LayerMask layerMask;
+	private RaycastHit hit;
+	private TransparentLogic opacityScript;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Squirrel = GameManager.Instance.charController.transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 squirrelPos = new Vector3 (Squirrel.position.x, Squirrel.position.y + 0.5f, Squirrel.position.z);
+		Vector3 dir = squirrelPos - transform.position;
+		Debug.DrawRay (transform.position, dir, Color.red);
+
+		if (Physics.Raycast (transform.position, dir, out hit, dir.magnitude, layerMask)) 
+		{
+			if (opacityScript == null) 
+			{
+				// Get the script component from the mesh
+				opacityScript = hit.collider.GetComponent <TransparentLogic> ();
+				// Set the isTransparent flag on the object
+				if (opacityScript != null) 
+				{
+					opacityScript.isTransparent = true;
+					print ("make transparent"); 
+				}
+			}
+		} 
+		// If raycast is not hitting any objects than set any of them that are currently transparent
+		// back to normal
+		else if (opacityScript != null) 
+		{
+			opacityScript.isTransparent = false;
+			opacityScript = null;
+		}
+	}
+
+
+}
---
+++ Suzie_Movement_Final2/Assets/TransparentLogic.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparentLogic : MonoBehaviour {
+
+	public bool isTransparent = false;
+
+	private float opacityLevel = 0.5f;
+	private MeshRenderer rend;
+	private Color oldColor;
+	private Color transparentColor;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rend = GetComponent<MeshRenderer> ();
+		oldColor = rend.sharedMaterial.color;
+		transparentColor = new Color (oldColor.r, oldColor.g, oldColor.b, opacityLevel);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!rend.isVisible)
+			return;
+			
+		if (isTransparent) {
+			ChangeOpacity (transparentColor);
+		} 
+		else 
+		{
+			ChangeOpacity (oldColor);
+		}
+			
+	}
+
+	void ChangeOpacity (Color color)
+	{
+		rend.sharedMaterial.SetColor ("_Color", color);
+	}
+}
