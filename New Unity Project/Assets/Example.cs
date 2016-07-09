072abebc24c42b2a906bc7d90ff41dc4f27e53fd
Roman <pencilking2002@gmail.com>
2016-07-08 20:04:06 -0400

    blah

---
+++ New Unity Project/Assets/Example.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+	public GameObject player;
+	public Rigidbody2D rb;
+	public float jumpForce = 200;
+
+	void Update()
+	{
+		print (Time.time);
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddForce(Vector2(0, jumpForce);
+	}
+
+
+}
